4ae3e1b684a09d2ef7a0913ec6ac8680c3d47b3d
Manel Pando <mpandofernandez@gmail.com>
2024-11-11 18:42:55 +0100

    Erased Godot 3.6

--- TheXQuest/Scripts/RayasGira_Intro.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite
-
-# Rotation speed in radians per second
-var rotation_speed = 12.0
-
-func _process(delta):
-	# Rotate the sprite
-	rotation += rotation_speed * delta	
--- TheXQuest/Scripts/Titulo.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-
-func _ready():
-
-	$Tween.interpolate_property(self, "percent_visible", 0.0, 1.0, 5.0, Tween.TRANS_CUBIC, Tween.EASE_IN)	
-	$Tween.start()
