c2c15359f39ac949de67a6f9faf157c24c810ea8
Manel Pando <mpandofernandez@gmail.com>
2024-12-17 16:30:18 +0100

    intentar arreglar combat

--- TheXQuest/Scripts/combate.gd
+++ TheXQuest/Scripts/combate.gd
@@ -15,48 +15,52 @@ var vidaInicial = Enemigo1.vida
 
 const scene_path = "res://Escenas/combate.tscn"
 
+
 func _ready():
+	
+	# Instanciar el enemigo
+	var enemigo_scene = preload("res://Escenas/EscenaEnemigo/Enemigo1.tscn")
+	
+	print(GameState.player_x)
+	print(GameState.player_y)
+	PlayerStats.get_player_stats()
+	actualizar_label_jugador()
+	actualizar_label_enemigo()
+	get_node("Jugador/AnimatedSprite2D").play("idle")
+	enemigo = enemigo_scene.instantiate()  # Instanciamos el enemigo
+	Enemigo1._ready()
+	
+	apareceDialogo("¡Un enemigo salvaje apareció!")
+	await get_tree().create_timer(2).timeout  # Esperar a que se muestre el diálogo
+	ocultarDialogo()
+	
 	MainTrans.show()
 	sceneTransitionAnimation.get_parent().get_node("ColorRect").color.a = 255
 	sceneTransitionAnimation.play("fade_in")
 	await get_tree().create_timer(0.5).timeout
 	MainTrans.hide()
 	AudioPlayer.play_music_battle(scene_path)
-	
-	# Instanciar el enemigo
-	Enemigo1._ready()
-	print(GameState.player_x)
-	print(GameState.player_y)
-	PlayerStats.get_player_stats()
-	actualizar_label_jugador()
-	actualizar_label_enemigo()
-	$Jugador/AnimatedSprite2D.play("idle")
-	var enemigo_scene = preload("res://Escenas/EscenaEnemigo/Enemigo1.tscn")
-	enemigo = enemigo_scene.instantiate()  # Instanciamos el enemigo
-	apareceDialogo("¡Un enemigo salvaje apareció!")
-	await get_tree().create_timer(2).timeout  # Esperar a que se muestre el diálogo
-	ocultarDialogo()
 
 func _process(delta: float) -> void:
-	if $Jugador/AnimatedSprite2D.animation=="idle":
-		$Jugador/AnimatedSprite2D.position=Vector2(823,608)
+	if get_node("Jugador/AnimatedSprite2D").animation=="idle":
+		get_node("Jugador/AnimatedSprite2D").position=Vector2(823,608)
 		
-	elif $Jugador/AnimatedSprite2D.animation=="attack":
-		$Jugador/AnimatedSprite2D.position=Vector2(823,585)
+	elif get_node("Jugador/AnimatedSprite2D").animation=="attack":
+		get_node("Jugador/AnimatedSprite2D").position=Vector2(823,585)
 		
-	elif $Jugador/AnimatedSprite2D.animation=="runAway":
-		$Jugador/AnimatedSprite2D.position=Vector2(823,604)
+	elif get_node("Jugador/AnimatedSprite2D").animation=="runAway":
+		get_node("Jugador/AnimatedSprite2D").position=Vector2(823,604)
 		
-	elif $Jugador/AnimatedSprite2D.animation=="victory":
-		$Jugador/AnimatedSprite2D.position=Vector2(823,566)
+	elif get_node("Jugador/AnimatedSprite2D").animation=="victory":
+		get_node("Jugador/AnimatedSprite2D").position=Vector2(823,566)
 		
 # Función para atacar al enemigo
 func atacar_enemigo():
 	Enemigo1.recibir_daño(15)
 	if Enemigo1.vida <= 0:
-		$Enemigo1.visible = false
+		get_node("Enemigo1").visible = false
 		apareceDialogo("Has ganado")
-		$Jugador/AnimatedSprite2D.play("victory")
+		get_node("Jugador/AnimatedSprite2D").play("victory")
 		await get_tree().create_timer(2.5).timeout
 		PlayerStats.add_exp(Enemigo1.vida/3)
 		apareceDialogo("Has ganado" +" "+str(vidaInicial/3)+"exp")
@@ -78,27 +82,29 @@ func atacar_enemigo():
 
 # Función para mostrar el diálogo
 func apareceDialogo(dialogo: String):
-	$Player2/VBoxContainer/btnFight.disabled = true
-	$Dialogo.visible = true
-	$Dialogo/Label.text = dialogo
+	get_node("Player2/VBoxContainer/btnFight").disabled = true
+	get_node("Dialogo").visible = true
+	get_node("Dialogo/Label").text = dialogo
 	dialogo_timer.start()  # Inicia el temporizador para ocultar el diálogo automáticamente
 
 # Función para ocultar el diálogo
 func ocultarDialogo():
-	$Dialogo.visible = false
-	$Player2/VBoxContainer/btnFight.disabled = false
+	get_node("Dialogo").visible = false
+	get_node("Player2/VBoxContainer/btnFight").disabled = false
 
 # Botón de ataque
 func _on_btn_fight_pressed() -> void:
+	print("boton presionado")
 	apareceDialogo("Has usado Ataque")
-	$Jugador/AnimatedSprite2D.play("attack")
+	get_node("Jugador/AnimatedSprite2D").play("attack")
 	await get_tree().create_timer(1).timeout  # Esperar a que el diálogo se muestre
 	atacar_enemigo()
 
 # Botón de huida
 func _on_btn_run_pressed() -> void:
+	print("boton presionado")
 	apareceDialogo("Has huido del combate")
-	$Jugador/AnimatedSprite2D.play("runAway")
+	get_node("Jugador/AnimatedSprite2D").play("runAway")
 	await get_tree().create_timer(2).timeout
 	
 	MainTrans.show()
@@ -117,10 +123,10 @@ func _on_btn_run_pressed() -> void:
 # Método que se llama cuando el temporizador termina
 func _on_dialogo_timer_timeout() -> void:
 	ocultarDialogo()
-	$Jugador/AnimatedSprite2D.play("idle")
+	get_node("Jugador/AnimatedSprite2D").play("idle")
 
 func actualizar_label_jugador():
-	$HBoxContainer/Player/LabelVidaJugador.text = "Nivel: " + str(PlayerStats.level) + " " + "Jugador 1 " + str(PlayerStats.health) + "/" + str(PlayerStats.maxHealth)
+	get_node("HBoxContainer/Player/LabelVidaJugador").text = "Nivel: " + str(PlayerStats.level) + " " + "Jugador 1 " + str(PlayerStats.health) + "/" + str(PlayerStats.maxHealth)
 	
 func enemigo_decision_combate():
 	var opcio = randi() % 3
@@ -132,7 +138,7 @@ func enemigo_decision_combate():
 		Enemigo1.curar(10)
 	elif (opcio == 2 && opcio2 == 89):
 		apareceDialogo("El enemigo ha huido")
-		get_tree().change_scene_to_file("res://Escenas/Test.tscn")
+		get_tree().change_scene_to_file("res://Escenas/forest/Test.tscn")
 	else:
 		apareceDialogo("El enemigo ha usado Ataque")
 		PlayerStats.recibir_daño(Enemigo1.ataque)
@@ -148,4 +154,8 @@ func enemigo_decision_combate():
 	
 
 func actualizar_label_enemigo():
-	$Enemigo1/LabelEnemigo.text = str(Enemigo1.vida)
+	get_node("Enemigo1/LabelEnemigo").text = str(Enemigo1.vida)
+
+
+func _on_btn_item_pressed() -> void:
+	pass # Replace with function body.
